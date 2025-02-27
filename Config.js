2e0998e08a370b425395eb60f8cd7605d58c7d3c
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-02-28 00:23:17 +0300

    Dragon 🐲

--- Config.js
+++ Config.js
@@ -9,23 +9,23 @@ SESSION_ID: process.env.SESSION_ID || "",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
-BOT_NAME: process.env.BOT_NAME || "✦ De ✦ Junior ✦ MD ✦",
+BOT_NAME: process.env.BOT_NAME || "✦ SNAPDRAGON ✦ JET ✦ MD ✦",
 // add bot namw here for menu
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "😁,💓,💪,💯,🎈,💀,🥰,😜,🥶,🤑,💥,🩵,🔥,✨,💫,🤘,👋",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "254704009677",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "✦ De ✦ Junior ✦ MD ✦",
+OWNER_NAME: process.env.OWNER_NAME || "🔸 SNAPDRAGON 🔸 JET ✦ MD ✦",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ✦ De ✦ Junior ✦ MD ✦*",
+DESCRIPTION: process.env.DESCRIPTION || "*© ✦ SNAPDRAGON ✦ JET ✦ MD ✦*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/v5t4ju.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> DE JUNIOR IS RUNNING ACTIVE AND ALIVE\n\n\nKEEP USING DE JUNIOR FROM JUNIOR TECH INC⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> SNAPDRAGON JET IS FLYING HIGH AND ALIVE \n\n\nKEEP USING SNAPDRAGON JET FROM SNAPDRAGON TECH INC⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -37,7 +37,7 @@ AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*🎉👀 Seen by DÉ JÚÑÍÓÚR 🚀🔥*",
+AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*VIEWED AND LIKED BY SNAPDRAGON ✔️💥*",
 // set the auto reply massage on status reply    
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
