8a6b040d20290203539b38bf392cd6e245e8303b
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-03-01 18:02:05 +0300

    Dragon 🐲

--- plugins/listcmd.js
+++ plugins/listcmd.js
@@ -6,7 +6,7 @@ cmd({
     alias: ["listcmd","commands"],
     desc: "menu the bot",
     category: "menu",
-    react: "🛩️",
+    react: "🚁",
     filename: __filename
 }, 
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
