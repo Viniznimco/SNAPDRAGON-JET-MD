b04623cd364c241f5f076201c93a0a1c8572c5fe
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-03-01 18:41:24 +0300

    Dragon 🐲

--- plugins/repo.js
+++ plugins/repo.js
@@ -7,30 +7,30 @@ cmd({
     alias: ["sc", "script", "info"],
     desc: "Info about the bot repository",
     category: "main",
-    react: "👨‍💻",
+    react: "✈️",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, reply }) => {
     try {
         // Fetch repository data from GitHub API
-        const repoResponse = await axios.get('https://github.com/Viniznimco/JUNIOR-MD');
+        const repoResponse = await axios.get('https://github.com/Viniznimco/SNAPDRAGON-JET-MD');
         const { stargazers_count, forks_count } = repoResponse.data;
         const userCount = forks_count * 5; // Estimate user count based on forks
 
         // Construct the message
         const message = `
-*Hello there, De Junior User! 👋*
+*Hello there, SNAPDRAGON JET User! 👋*
 
 💻 *De Junior MD Repository Info*:
 ⭐ *Stars*: ${stargazers_count}
 🍴 *Forks*: ${forks_count}
 👥 *Users*: ${userCount}
-🔗 *Repository*: https://github.com/Viniznimco/JUNIOR-MD
-> ✨ De Junior WhatsApp Bot – Simple. Smart. Feature-packed. 🚀
+🔗 *Repository*: https://github.com/Viniznimco/SNAPDRAGON-JET-MD
+> ✨ SNAPDRAGON JET WhatsApp Bot – Simple. Smart. Feature-packed. 🚀
 Effortlessly elevate your WhatsApp experience with our cutting-edge bot technology! 🎊
 *💡 Tip: Don’t forget to fork the repo and leave a star to show your support! 🌟*
 
-🙌 Thank you for choosing De Junior MD – your ultimate bot companion! 🎉
+🙌 Thank you for choosing SNAPDRAGON JET MD – your ultimate bot companion! 🎉
         `;
 
         // Send the repository info as a text message
@@ -40,7 +40,7 @@ Effortlessly elevate your WhatsApp experience with our cutting-edge bot technolo
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/v5t4ju.jpg` },
+                image: { url: `https://files.catbox.moe/xb17f0.jpg` },
                 caption: message,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -48,7 +48,7 @@ Effortlessly elevate your WhatsApp experience with our cutting-edge bot technolo
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363200367779016@newsletter',
-                        newsletterName: 'DE JUNIOR MD🤍❤️',
+                        newsletterName: 'SNAPDRAGON JET MD✈️'💓,
                         serverMessageId: 143
                     }
                 }
