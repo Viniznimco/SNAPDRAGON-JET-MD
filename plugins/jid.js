775bc02d197555ca8242b926693e4676e1c57ea6
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-03-01 17:57:26 +0300

    Dragon 🐲

--- plugins/jid.js
+++ plugins/jid.js
@@ -3,7 +3,7 @@ const { cmd } = require('../command');
 cmd({
     pattern: "jid1",
     desc: "Get the JID of the user or group.",
-    react: "📍",
+    react: "🛩️",
     category: "group",
     filename: __filename,
 }, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
