fecd549a793c632c25b78cf9f400ae4f4e419798
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-03-02 10:57:32 +0300

    Dragon 🐲

--- plugins/tts.js
+++ plugins/tts.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *DE JUNIOR TRANSLATION*
+        const translationMessage = `> *SNAPDRAGON JET TRANSLATION*
 
 > 🔤 *Original*: ${textToTranslate}
 
