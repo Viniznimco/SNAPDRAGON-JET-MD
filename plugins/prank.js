0a286cbbc5396fb367bee5796bf835036fa7b368
Viniznimco <clintoncheruiyotlangat@gmail.com>
2025-03-26 10:22:35 +0300

    SNAPDRAGON 🎈

--- plugins/prank.js
+++ plugins/prank.js
@@ -35,7 +35,7 @@ async (conn, mek, m, { from, quoted, reply }) => {
             '⚠️ _This is a simulated hacking activity for entertainment purposes._',
             '⚠️ _Remember: Ethical hacking ensures safety._',
             '',
-            '> *CASEYRHODES XMD: HACKING SIMULATION COMPLETE* ☣'
+            '> *SNAPDRAGON-JET-MD: HACKING SIMULATION COMPLETE* ☣'
         ];
 
         for (const step of steps) {
